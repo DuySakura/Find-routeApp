ce525818dfa35cf4b730e8de049706be7b72bfbf
DuySakura <duysakura25@gmail.com>
2025-07-10 03:47:12 +0700

    update side bar

--- static/scripts.js
+++ static/scripts.js
@@ -169,8 +169,8 @@ function resetGraph() {
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     const main = document.getElementById('main');
-    sidebar.classList.toggle('hidden');
-    main.classList.toggle('full');
+    sidebar.classList.toggle('appear');
+    main.classList.toggle('minimize');
 }
 
 
